b3d098a1f3f78dee2253f5335f2c8bff3f910be1
Marcus Williams <marcus@dottodotdesign.co.uk>
2020-06-17 08:30:46 +0100

    Update selector

--- src/index.js
+++ src/index.js
@@ -350,7 +350,7 @@ class Paragraph {
     _toggleTune(tune) {
         this.data.alignment = tune;
         this.settings.forEach((tune) => {
-            this.wrapper.classList.toggle(tune.name, !!this.data[tune.name]);
+            this._element.classList.toggle(tune.name, !!this.data[tune.name]);
         });
     }
 
