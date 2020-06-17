c8b20b2d9b7966aecb63ee2914c76494d97abe9c
Marcus Williams <marcus@dottodotdesign.co.uk>
2020-06-17 08:39:57 +0100

    Add align class

--- src/index.js
+++ src/index.js
@@ -350,10 +350,9 @@ class Paragraph {
     _toggleTune(tune) {
         this.data.alignment = tune;
         this.settings.forEach((tune) => {
-            console.log(tune.name, !!this.data.alignment);
-
-            this._element.classList.toggle(tune.name, !!this.data.alignment);
+            this._element.classList.remove(tune.name);
         });
+        this._element.classList.add(this.data.alignment);
     }
 
     /**
