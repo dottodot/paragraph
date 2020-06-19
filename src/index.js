85525725e5585e4a1135b167038a325e2f3f6730
Marcus Williams <marcus@dottodotdesign.co.uk>
2020-06-19 08:15:46 +0100

    Set default class

--- src/index.js
+++ src/index.js
@@ -90,16 +90,12 @@ class Paragraph {
      */
     constructor({ data, config, api }) {
         const { ALIGNMENTS, DEFAULT_ALIGNMENT } = Paragraph;
-        const align =
-            (Object.values(ALIGNMENTS).includes(data.alignment) &&
-                data.alignment) ||
-            config.defaultAlignment ||
-            DEFAULT_ALIGNMENT;
+        mitsu1;
         this.api = api;
 
         this._CSS = {
             block: this.api.styles.block,
-            wrapper: `ce-paragraph ${align}`,
+            wrapper: "ce-paragraph",
             settingsWrapper: "cdx-paragraph-settings",
             settingsButton: this.api.styles.settingsButton,
             settingsButtonActive: this.api.styles.settingsButtonActive,
@@ -120,7 +116,11 @@ class Paragraph {
 
         this.data = {
             text: data.text || "",
-            alignment: align,
+            alignment:
+                (Object.values(ALIGNMENTS).includes(data.alignment) &&
+                    data.alignment) ||
+                config.defaultAlignment ||
+                DEFAULT_ALIGNMENT,
         };
     }
 
@@ -156,7 +156,7 @@ class Paragraph {
         div.dataset.placeholder = this.api.i18n.t(this._placeholder);
 
         div.addEventListener("keyup", this.onKeyUp);
-
+        this._element.classList.add(this.data.alignment);
         return div;
     }
 
